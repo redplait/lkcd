c806a6d040d7ee79707e08af10c0298be772124c
redplait <>
2021-08-24 11:56:21 +0300

    -r option to check .rodata

--- lkrd/lkmem.cc
+++ lkrd/lkmem.cc
@@ -37,6 +37,7 @@ void usage(const char *prog)
   printf("-c - check memory. Achtung - you must first load lkcd driver\n");
   printf("-d - use disasm\n");
   printf("-f - dump ftraces\n");
+  printf("-r - check .rodata section\n");
   printf("-v - verbose mode\n");
   exit(6);
 }
@@ -175,6 +176,88 @@ void dump_patched(a64 curr_addr, char *ptr, char *arg, sa64 delta)
      printf("mem at %p patched to %p\n", ptr, arg);
 }
 
+void dump_and_check(int fd, int opt_c, sa64 delta, int has_syms, std::map<a64, a64> &filled)
+{
+  for ( auto &c: filled )
+  {
+    auto curr_addr = c.first;
+    auto addr = c.second;
+    if ( g_opt_v )
+    {
+      size_t off = 0;
+      const char *name = lower_name_by_addr_with_off(curr_addr, &off);
+      if ( name != NULL )
+      {
+         const char *pto = name_by_addr(addr);
+         if ( pto != NULL )
+         {
+           if ( off )
+             printf("# %s+%lX -> %s\n", name, off, pto);
+           else
+             printf("# %s -> %s\n", name, pto);
+           } else {
+             if ( off )
+               printf("# %s+%lX\n", name, off);
+             else
+               printf("# %s\n", name);
+           }
+         }
+         printf("%p\n", (void *)curr_addr);
+      }
+#ifndef _MSC_VER
+      if ( opt_c )
+      {
+         char *ptr = (char *)curr_addr + delta;
+         char *arg = ptr;
+         int err = ioctl(fd, IOCTL_READ_PTR, (int *)&arg);
+         if ( err )
+         {
+           printf("read at %p failed, error %d (%s)\n", ptr, errno, strerror(errno));
+           continue;
+         }
+         char *real = (char *)addr + delta;
+         if ( real != arg )
+         {
+           if ( is_inside_kernel((unsigned long)arg) )
+           {
+              if ( !has_syms )
+                printf("mem at %p: %p (must be %p)\n", ptr, arg, real);
+              else 
+              {
+                size_t off = 0;
+                const char *name = lower_name_by_addr_with_off(curr_addr, &off);
+                if ( name != NULL )
+                {
+                  const char *pto = name_by_addr((a64)(arg - delta));
+                  if ( pto != NULL )
+                  {
+                     if ( off )
+                       printf("mem at %p (%s+%lX) patched to %p (%s)\n", ptr, name, off, arg, pto);
+                     else
+                       printf("mem at %p (%s) patched to %p (%s)\n", ptr, name, arg, pto);
+                   } else {
+                     if ( off )
+                       printf("mem at %p (%s+%lX) patched to %p\n", ptr, name, off, arg);
+                     else
+                       printf("mem at %p (%s) patched to %p\n", ptr, name, arg);
+                   }
+                } else
+                   printf("mem at %p: %p (must be %p)\n", ptr, arg, real);
+              }
+           } else 
+           { // address not in kernel
+              const char *mname = find_kmod((unsigned long)arg);
+              if ( mname )
+                printf("mem at %p: %p (must be %p) - patched by %s\n", ptr, arg, real, mname);
+              else
+                printf("mem at %p: %p (must be %p) - patched by UNKNOWN\n", ptr, arg, real);
+            }
+         }
+      } /* opt_c */
+#endif /* !_MSC_VER */
+  }
+}
+
 int is_nop(unsigned char *body)
 {
   // nop dword ptr [rax+rax+00h] - 0F 1F 44 00 00
@@ -202,12 +285,13 @@ int main(int argc, char **argv)
    int opt_f = 0,
        opt_d = 0,
        opt_c = 0,
+       opt_r = 0,
        opt_b = 0;
    int c;
    int fd = 0;
    while (1)
    {
-     c = getopt(argc, argv, "bcfvd");
+     c = getopt(argc, argv, "bcdfrv");
      if (c == -1)
 	break;
 
@@ -227,6 +311,9 @@ int main(int argc, char **argv)
          break;
         case 'c':
           opt_c = 1;
+         break;
+        case 'r':
+          opt_r = 1;
          break;
         default:
          usage(argv[0]);
@@ -265,8 +352,8 @@ int main(int argc, char **argv)
      }
      has_syms = 1;
    }
-#ifndef _MSC_VER
    sa64 delta = 0;
+#ifndef _MSC_VER
    // open driver
    if ( opt_c ) 
    {
@@ -389,6 +476,41 @@ end:
    for ( Elf_Half i = 0; i < n; ++i ) 
    {
      section* sec = reader.sections[i];
+     if ( opt_r && sec->get_name() == ".rodata" )
+     {
+       std::map<a64, a64> filled;
+       auto off = sec->get_offset();
+       printf(".rodata section offset %lX\n", off);
+       size_t count = 0;
+       a64 curr_addr;
+       // under arm64 we need count relocs in .data section       
+       if ( reader.get_machine() == 183 )
+       {
+         a64 dstart = (a64)sec->get_address();
+         count = filter_arm64_relocs(reader, dstart, dstart + sec->get_size(), (a64)text_start, (a64)(text_start + text_size), filled);
+       } else {
+         a64 *curr = (a64 *)sec->get_data();
+         a64 *end  = (a64 *)((char *)curr + sec->get_size());
+         curr_addr = sec->get_address();
+         const endianess_convertor &conv = reader.get_convertor();
+         for ( ; curr < end; curr++, curr_addr += sizeof(a64) )
+         {
+           auto addr = conv(*curr);
+           if ( addr >= (a64)text_start &&
+                addr < (a64)(text_start + text_size)
+              )
+           {
+             count++;
+             filled[curr_addr] = addr;
+           }
+         }
+       }
+       printf("found in .rodata %ld\n", count);
+       // dump or check collected addresses
+       if ( g_opt_v || opt_c )
+         dump_and_check(fd, opt_c, delta, has_syms, filled);
+       continue;
+     }
      if ( sec->get_name() == ".data" )
      {
        std::map<a64, a64> filled;
@@ -421,177 +543,102 @@ end:
        printf("found %ld\n", count);
        // dump or check collected addresses
        if ( g_opt_v || opt_c )
-         for ( auto &c: filled )
-         {
-            curr_addr = c.first;
-            auto addr = c.second;
-            if ( g_opt_v )
+         dump_and_check(fd, opt_c, delta, has_syms, filled);
+       if ( opt_d )
+       {
+          x64_disasm dis(text_start, text_size, text_section->get_data(), sec->get_address(), sec->get_size());
+          // find bss if we need
+          if ( opt_b )
+          {
+            for ( Elf_Half j = 0; j < n; ++j )
             {
-               size_t off = 0;
-               const char *name = lower_name_by_addr_with_off(curr_addr, &off);
-               if ( name != NULL )
-               {
-                 const char *pto = name_by_addr(addr);
-                 if ( pto != NULL )
-                 {
-                   if ( off )
-                     printf("# %s+%lX -> %s\n", name, off, pto);
-                   else
-                     printf("# %s -> %s\n", name, pto);
-                 } else {
-                   if ( off )
-                     printf("# %s+%lX\n", name, off);
-                   else
-                     printf("# %s\n", name);
-                 }
-               }
-               printf("%p\n", (void *)curr_addr);
+              section* s = reader.sections[j];
+              if ( (s->get_type() & SHT_NOBITS) && 
+                   (s->get_name() == ".bss" )
+                 )
+              {
+                a64 bss_addr = s->get_address();
+                if ( g_opt_v )
+                  printf(".bss address %p size %lX\n", (void *)bss_addr, s->get_size());
+                dis.set_bss(bss_addr, s->get_size());
+                break;
+              }
             }
-#ifndef _MSC_VER
-            if ( opt_c )
+          }
+          // fill indirect thunks
+          for ( auto &c: s_x64_thunks )
+          {
+            a64 thunk_addr = get_addr(c.name);
+            if ( !thunk_addr )
+              printf("cannot find %s\n", c.name);
+            else
+              dis.set_indirect_thunk(thunk_addr, c.reg);
+          }
+          std::set<a64> out_res;
+          size_t tcount = 0;
+          struct addr_sym *tsyms = get_in_range(text_start, text_start + text_size, &tcount);
+          if (tsyms != NULL)
+          {
+            for (size_t i = 0; i < tcount; i++)
+              dis.process(tsyms[i].addr, filled, out_res);
+            free(tsyms);
+          }
+          else
+          {
+            // now disasm some funcs - security_load_policy
+            a64 faddr = get_addr("rcu_sched_clock_irq");
+            if (faddr)
             {
-               char *ptr = (char *)curr_addr + delta;
-               char *arg = ptr;
-               int err = ioctl(fd, IOCTL_READ_PTR, (int *)&arg);
-               if ( err )
-                 printf("read at %p failed, error %d (%s)\n", ptr, errno, strerror(errno));
-               else
-               {
-                 char *real = (char *)addr + delta;
-                 if ( real != arg )
-                 {
-                   if ( is_inside_kernel((unsigned long)arg) )
-                   {
-                     if ( !has_syms )
-                       printf("mem at %p: %p (must be %p)\n", ptr, arg, real);
-                     else {
-                       size_t off = 0;
-                       const char *name = lower_name_by_addr_with_off(curr_addr, &off);
-                       if ( name != NULL )
-                       {
-                         const char *pto = name_by_addr((a64)(arg - delta));
-                         if ( pto != NULL )
-                         {
-                           if ( off )
-                             printf("mem at %p (%s+%lX) patched to %p (%s)\n", ptr, name, off, arg, pto);
-                           else
-                             printf("mem at %p (%s) patched to %p (%s)\n", ptr, name, arg, pto);
-                         } else {
-                           if ( off )
-                             printf("mem at %p (%s+%lX) patched to %p\n", ptr, name, off, arg);
-                           else
-                             printf("mem at %p (%s) patched to %p\n", ptr, name, arg);
-                         }
-                       } else
-                           printf("mem at %p: %p (must be %p)\n", ptr, arg, real);
-                     }
-                   } else {
-                     const char *mname = find_kmod((unsigned long)arg);
-                     if ( mname )
-                       printf("mem at %p: %p (must be %p) - patched by %s\n", ptr, arg, real, mname);
-                     else
-                       printf("mem at %p: %p (must be %p) - patched by UNKNOWN\n", ptr, arg, real);
-                   }
-                 }
-               }
-             } /* opt_c */
-#endif /* !_MSC_VER */
-         }
-         if ( opt_d )
-         {
-           x64_disasm dis(text_start, text_size, text_section->get_data(), sec->get_address(), sec->get_size());
-           // find bss if we need
-           if ( opt_b )
-           {
-             for ( Elf_Half j = 0; j < n; ++j )
-             {
-               section* s = reader.sections[j];
-               if ( (s->get_type() & SHT_NOBITS) && 
-                    (s->get_name() == ".bss" )
-                  )
-               {
-                 a64 bss_addr = s->get_address();
-                 if ( g_opt_v )
-                   printf(".bss address %p size %lX\n", (void *)bss_addr, s->get_size());
-                 dis.set_bss(bss_addr, s->get_size());
-                 break;
-               }
-             }
-           }
-           // fill indirect thunks
-           for ( auto &c: s_x64_thunks )
-           {
-             a64 thunk_addr = get_addr(c.name);
-             if ( !thunk_addr )
-               printf("cannot find %s\n", c.name);
-             else
-               dis.set_indirect_thunk(thunk_addr, c.reg);
-           }
-           std::set<a64> out_res;
-           size_t tcount = 0;
-           struct addr_sym *tsyms = get_in_range(text_start, text_start + text_size, &tcount);
-           if (tsyms != NULL)
-           {
-             for (size_t i = 0; i < tcount; i++)
-               dis.process(tsyms[i].addr, filled, out_res);
-             free(tsyms);
-           }
-           else
-           {
-             // now disasm some funcs - security_load_policy
-             a64 faddr = get_addr("rcu_sched_clock_irq");
-             if (faddr)
-             {
-               dis.process(faddr, filled, out_res);
-             }
-           }
-           printf("found with disasm: %ld\n", out_res.size());
-           if ( g_opt_v )
-           {
-             for ( auto c: out_res )
-             {
-               size_t off = 0;
-               const char *name = lower_name_by_addr_with_off(c, &off);
-               if ( name != NULL )
-               {
-                 if ( off )
-                   printf("# %s+%lX\n", name, off);
-                 else
-                   printf("# %s\n", name);
-               }
-               printf("%p\n", (void *)c);
-             }
-           }
+              dis.process(faddr, filled, out_res);
+            }
+          }
+          printf("found with disasm: %ld\n", out_res.size());
+          if ( g_opt_v )
+          {
+            for ( auto c: out_res )
+            {
+              size_t off = 0;
+              const char *name = lower_name_by_addr_with_off(c, &off);
+              if ( name != NULL )
+              {
+                if ( off )
+                  printf("# %s+%lX\n", name, off);
+                else
+                  printf("# %s\n", name);
+              }
+              printf("%p\n", (void *)c);
+            }
+          }
 #ifndef _MSC_VER
-           if ( opt_c )
-           {
-             for ( auto c: out_res )
-             {
-               char *ptr = (char *)c + delta;
-               char *arg = ptr;
-               int err = ioctl(fd, IOCTL_READ_PTR, (int *)&arg);
-               if ( err )
-                 printf("read at %p failed, error %d (%s)\n", ptr, errno, strerror(errno));
-               else if ( arg != NULL )
-               {
-                  if ( is_inside_kernel((unsigned long)arg) )
-                  {
-                     if ( !has_syms )
-                       printf("mem at %p: %p\n", ptr, arg);
-                     else
-                       dump_patched(c, ptr, arg, delta);
-                  } else {
-                     const char *mname = find_kmod((unsigned long)arg);
-                     if ( mname )
-                       printf("mem at %p: %p - patched by %s\n", ptr, arg, mname);
-                     else
-                       printf("mem at %p: %p - patched by UNKNOWN\n", ptr, arg);
-                  }
-               }
-             }
-           } // opt_c
+          if ( opt_c )
+          {
+            for ( auto c: out_res )
+            {
+              char *ptr = (char *)c + delta;
+              char *arg = ptr;
+              int err = ioctl(fd, IOCTL_READ_PTR, (int *)&arg);
+              if ( err )
+                printf("read at %p failed, error %d (%s)\n", ptr, errno, strerror(errno));
+              else if ( arg != NULL )
+              {
+                 if ( is_inside_kernel((unsigned long)arg) )
+                 {
+                    if ( !has_syms )
+                      printf("mem at %p: %p\n", ptr, arg);
+                    else
+                      dump_patched(c, ptr, arg, delta);
+                 } else {
+                    const char *mname = find_kmod((unsigned long)arg);
+                    if ( mname )
+                      printf("mem at %p: %p - patched by %s\n", ptr, arg, mname);
+                    else
+                      printf("mem at %p: %p - patched by UNKNOWN\n", ptr, arg);
+                 }
+              }
+            }
+          } // opt_c
 #endif /* !_MSC_VER */
-         } // opt_d
+       } // opt_d
        break;
      }
    }
