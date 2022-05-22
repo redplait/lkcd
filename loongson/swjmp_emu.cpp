1b99740bdde53577b34dd9d7b2d94aa4acf64a06
redplait <>
2022-05-22 21:56:55 +0300

    4rth variant of switch table

--- loongson/swjmp_emu.cpp
+++ loongson/swjmp_emu.cpp
@@ -10,6 +10,158 @@
 extern int is_pcadd(int itype);
 extern ea_t pcadd(int itype, ea_t pc, int imm);
 
+bool loongson_jump_pattern9ops::is_ret()
+{
+  if ( insn.itype != Loong_jirl )
+    return false;
+  trackop(insn.Op2, regA);
+  return true;
+}
+
+// add.d regA, rBase, rJ
+bool loongson_jump_pattern9ops::is_add()
+{
+  if ( insn.itype != Loong_add_d || !same_value(insn.Op1, regA) )
+    return false;
+#ifdef _DEBUG
+  msg("9pi1: %a\n", insn.ea);
+#endif
+  trackop(insn.Op2, rBase);
+  trackop(insn.Op3, rJ);
+  return true;
+}
+
+// ldx.d rJ, rBase, rS
+bool loongson_jump_pattern9ops::is_ldx()
+{
+  if ( insn.itype != Loong_ldx_d || !same_value(insn.Op1, rJ) )
+    return false;
+  if ( same_value(insn.Op2, rBase) )
+  {
+    trackop(insn.Op3, rS);
+// #ifdef _DEBUG
+    msg("9pi2: %a rS3 %d\n", insn.ea, insn.Op3.reg);
+// #endif
+    return true;
+  } else if ( same_value(insn.Op3, rBase) )
+  {
+    trackop(insn.Op2, rS);
+// #ifdef _DEBUG
+    msg("9pi2: %a rS2 %d\n", insn.ea, insn.Op2.reg);
+// #endif
+    return true;
+  } else
+    return false;
+}
+
+// alsl.d rS, rIdx2, r0, size
+bool loongson_jump_pattern9ops::is_alsl0()
+{
+  if ( insn.itype != Loong_alsl_d || !same_value(insn.Op1, rS) || insn.Op3.reg )
+    return false;
+// #ifdef _DEBUG
+  msg("9pi3: %a\n", insn.ea);
+// #endif
+  trackop(insn.Op2, rIdx2); 
+  si->set_jtable_element_size(1 << insn.Op4.value);
+  return true;
+}
+
+// bstrpick rIdx2, rIdx
+bool loongson_jump_pattern9ops::is_bstrpick()
+{
+  if ( (insn.itype != Loong_bstrpick_w && insn.itype != Loong_bstrpick_d) || !same_value(insn.Op1, rIdx2) )
+    return false;
+// #ifdef _DEBUG
+  msg("9pi4: %a\n", insn.ea);
+// #endif
+  trackop(insn.Op2, rIdx); 
+  return true;
+}
+
+// addi.d rBase, rBase2, offset
+bool loongson_jump_pattern9ops::is_addi()
+{
+  if ( insn.itype != Loong_addi_d || !same_value(insn.Op1, rBase) )
+    return false;
+// #ifdef _DEBUG
+  msg("9pi5: %a\n", insn.ea);
+// #endif
+  trackop(insn.Op2, rBase2); 
+  add_off = insn.Op3.value;
+  return true;
+}
+
+// pcadduXXi rBase2, base
+bool loongson_jump_pattern9ops::is_pcadduXXi()
+{
+  if ( !same_value(insn.Op1, rBase2) )
+    return false;
+  if ( !is_pcadd(insn.itype) )
+    return false;
+  // ok, we finally have address of table
+  si->jumps = add_off + pcadd(insn.itype, insn.ea, insn.Op2.value);
+  si->set_elbase(si->jumps);
+// #ifdef _DEBUG
+  msg("9pi6: %a jumps %a\n", insn.ea, si->jumps);
+// #endif
+  return true;
+}
+
+// bltu rMax, rIdx default addr
+bool loongson_jump_pattern9ops::is_bxx()
+{
+ if ( insn.itype == Loong_bltu || insn.itype == Loong_blt )
+ {
+   if ( !same_value(insn.Op2, rIdx) )
+     return false;
+   trackop(insn.Op1, rMax); 
+ } else if ( insn.itype == Loong_bge || insn.itype == Loong_bgeu )
+ {
+   if ( !same_value(insn.Op1, rIdx) )
+     return false;
+   trackop(insn.Op2, rMax); 
+   is_ge = 1;
+ } else
+   return false;
+  si->defjump = insn.Op3.addr;
+  return true;
+}
+
+// mov rMax, imm
+bool loongson_jump_pattern9ops::is_rimm()
+{
+ if ( insn.itype != Loong_mov || !same_value(insn.Op1, rMax) )
+   return false;
+ auto jsize = insn.Op2.value;
+ if ( !is_ge )
+   jsize++;
+ si->set_jtable_size(jsize);
+ si->flags |= SWI_SIGNED;
+ si->startea = insn.ea;
+msg("9pi9: %a jumps %a size %d elsize %d\n", insn.ea, si->jumps, si->get_jtable_size(), si->get_jtable_element_size());
+ return true;
+}
+
+bool loongson_jump_pattern9ops::handle_mov(tracked_regs_t &_regs)
+{
+  if ( insn.itype != Loong_mov
+    && insn.Op1.type != o_reg
+    && insn.Op2.type != o_reg )
+  {
+    return false;
+  }
+  return set_moved(insn.Op2, insn.Op1, _regs);
+}
+
+bool loongson_jump_pattern9ops::finish()
+{
+#ifdef _DEBUG
+  msg("finish: %a\n", eas[8]);
+#endif
+  return eas[8] != BADADDR;
+}
+
 // 0) ret regA
 bool loongson_jump_pattern_t::is_ret(void)
 {
@@ -193,6 +345,7 @@ int loongson_is_switch(switch_info_t *si, const insn_t &insn)
     is_jump_pattern<loongson_jump_pattern_t1>,
     is_jump_pattern<loongson_jump_pattern_t2>,
     is_jump_pattern<loongson_jump_pattern_t3>,
+    is_jump_pattern<loongson_jump_pattern_t4>,
   };
   return check_for_table_jump(si, insn, patterns, qnumber(patterns));
 }
--- loongson/swjmp_emu.h
+++ loongson/swjmp_emu.h
@@ -112,3 +112,73 @@ class loongson_jump_pattern_t3: public loongson_jump_pattern_t
   virtual bool jpi2(void) override { return is_ldptr(); }
   virtual bool jpi1(void) override { return is_add_base(); }
 };
+
+// jump pattern #4
+// 8  mov rMax, imm
+// 7  bltu rMax, rIdx default addr
+// 6  pcadduXXi rBase2, base
+// 5  addi.d rBase, rBase2, offset
+// 4  bstrpick rIdx2, rIdx
+// 3  alsl.d rS, rIdx2, r0, size
+// 2  ldx.d rJ, rBase, rS
+// 1  add.d regA, rBase, rJ
+// 0  ret regA
+static const char lsw2_depends[][4] =
+{
+  { 1 },  // 0
+  { 2 },  // 1
+  { 3 },  // 2
+  { 4 },  // 3
+  { 5 },  // 4
+  { 6 },  // 5
+  { 7 },  // 6
+  { 8 },  // 7
+  { 0 },
+};
+
+class loongson_jump_pattern9ops : public jump_pattern_t
+{
+protected:
+  enum {
+   regA, rJ, rS, rIdx, rIdx2, rBase, rBase2, rMax
+  };
+  ea_t add_off;
+  int is_ge; // for bge(u)
+  // common methods
+  bool is_ret();
+  bool is_add();
+  bool is_ldx();
+  bool is_alsl0();
+  bool is_bstrpick();
+  bool is_addi();
+  bool is_pcadduXXi();
+  bool is_bxx();
+  bool is_rimm();
+public:
+  loongson_jump_pattern9ops(switch_info_t *_si)
+   : jump_pattern_t(_si, lsw2_depends, rMax)
+  {
+    add_off = NULL;
+    is_ge = 0;
+  }
+  virtual bool handle_mov(tracked_regs_t &_regs) override;
+  bool finish();
+  virtual bool jpi0(void) override { return is_ret(); }
+};
+
+class loongson_jump_pattern_t4: public loongson_jump_pattern9ops
+{
+ public:
+   loongson_jump_pattern_t4(switch_info_t *_si)
+     : loongson_jump_pattern9ops(_si)
+   {}
+  // 1..8
+  virtual bool jpi8(void) override { return is_rimm(); }
+  virtual bool jpi7(void) override { return is_bxx(); }
+  virtual bool jpi6(void) override { return is_pcadduXXi(); }
+  virtual bool jpi5(void) override { return is_addi(); }
+  virtual bool jpi4(void) override { return is_bstrpick(); }
+  virtual bool jpi3(void) override { return is_alsl0(); }
+  virtual bool jpi2(void) override { return is_ldx(); }
+  virtual bool jpi1(void) override { return is_add(); }
+};
