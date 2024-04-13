6bfa88c548ce604c33fabf8d080ed97ef407e215
redp <redplait@gmail.com>
2024-04-13 19:31:49 +0300

    -d option

--- inject/test.cc
+++ inject/test.cc
@@ -1,14 +1,31 @@
 #include <string>
+#include <getopt.h>
 #include <iostream>
 #include <string.h>
 #include <dlfcn.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <net/if.h>
+#include <linux/genetlink.h>
+#include <sys/ioctl.h>
+#include "../shared.h"
+
+int g_fd = -1;
 
 const unsigned char payload[] = {
 #include "hm.inc"
 };
+
+void usage(const char *prog)
+{
+  printf("%s usage: [options]\n", prog);
+  printf("Options:\n");
+  printf("-d - test-case with driver\n");
+  exit(6);
+}
 
 int inject()
 {
@@ -27,7 +44,15 @@ int inject()
    return 0;
  }
  // alloc
- char *alloced = (char *)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+ char *alloced = nullptr;
+ if ( g_fd != -1 )
+ {
+   unsigned long args[2] = { 4096, PROT_READ | PROT_WRITE };
+   int err = ioctl(g_fd, IOCTL_TEST_MMAP, (int *)args);
+   alloced = (!err) ? (char *)args[0] : (char *)MAP_FAILED;
+ } else {
+   alloced = (char *)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+ }
  if ( MAP_FAILED == alloced )
  {
    printf("cannot mmap, errno %d (%s)", errno, strerror(errno));
@@ -55,19 +80,50 @@ int inject()
  tab[4] = dlsym(c, "dlopen");
  tab[5] = dlsym(c, "dlsym");
  // mprotect
- if ( mprotect(alloced, 4096, PROT_READ | PROT_EXEC ) )
+ if ( g_fd != -1 )
  {
-   printf("cannot mprotect, errno %d (%s)", errno, strerror(errno));
-   return 0;
+   unsigned long args[3] = { (unsigned long)alloced, 4096, PROT_READ | PROT_EXEC };
+   int err = ioctl(g_fd, IOCTL_TEST_MPROTECT, (int *)args);
+printf("err %d\n", err);
+   if ( err ) goto emprot;
+ } else {
+   if ( mprotect(alloced, 4096, PROT_READ | PROT_EXEC ) ) goto emprot;
  }
  // set hooks
  *(void **)sym_m = alloced;
  *(void **)sym_f = alloced + 9;
  return 1;
+emprot:
+   printf("cannot mprotect, errno %d (%s)", errno, strerror(errno));
+   return 0;
 }
 
-int main()
+void open_driver()
 {
+  if ( g_fd != -1 ) return;
+  g_fd = open("/dev/lkcd", 0);
+  if ( g_fd == -1 )
+  {
+    printf("cannot open driver, error %d (%s)\n", errno, strerror(errno));
+    exit(errno);
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  int c;
+  while(1)
+  {
+    c = getopt(argc, argv, "dp:i:");
+    if ( c == -1 ) break;
+    switch(c)
+    {
+      case 'd': open_driver();
+        break;
+      default: usage(argv[0]);
+    }
+  }
   if ( !inject() ) return 1;
   printf("pid %d\n", getpid());
   do {
