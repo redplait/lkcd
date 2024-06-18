bd26d8b151a68011756625d982f81bff62a75530
redp <redplait@gmail.com>
2024-06-18 16:09:54 +0300

    more test cases

--- inject/test/fc.c
+++ inject/test/fc.c
@@ -12,6 +12,7 @@ double senseless_calc(int _i)
   printf(fmt_msg, _i);
   for ( i = 0; i < _i && i < sizeof(s_arr) / sizeof(s_arr[0]); i++ )
   {
+    printf("shared %f\n", res);
     res += asin(non_static_arr[i]) + atan(s_arr[i]);
   }
   printf("const string %f\n", res);
@@ -20,6 +21,25 @@ double senseless_calc(int _i)
 
 double __attribute__ ((section (".init.text"))) fake_init(int i)
 {
+  double res;
   printf("fake_init called, arg %d\n", i);
-  return senseless_calc(i);
+  res = senseless_calc(i);
+  if ( res < 0.1 )
+    printf("shared %f\n", res);
+  else
+    printf("missed\n");
+}
+
+double __attribute__ ((section (".init.text"))) do_in_init(int i)
+{
+  double res;
+  if ( i > 5 )
+    printf("missed\n");
+  else
+    printf("some uniq inside functiop\n");
+  printf("fake_do_in_init called, arg %d\n", i);
+  res = senseless_calc(i);
+  printf("shared %f\n", res);
+  if ( res > 0.1 )
+    printf("some uniq inside functiop\n");
 }
