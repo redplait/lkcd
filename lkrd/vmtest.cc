611fae6e59d912b2eecf3fb2ad744a623deaa640
redp <redplait@gmail.com>
2024-08-18 16:22:26 +0300

    add getopt

--- lkrd/vmtest.cc
+++ lkrd/vmtest.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <list>
+#include <getopt.h>
 #include <elfio/elfio_dump.hpp>
 #include "ksyms.h"
 #include "x64_disasm.h"
@@ -12,19 +13,43 @@ using namespace ELFIO;
 
 int g_opt_d = 0;
 
+void usage(const char *prog)
+{
+  printf("%s usage: [options] elf-file [symbols]\n", prog);
+  printf("Options:\n");
+  printf("-d - show disasm\n");
+  printf("-l - LSM hooks\n");
+  exit(6);
+}
+
+void rcf(const char *name)
+{
+  fprintf(stderr, "cannot find %s\n", name);
+}
+
 int main(int argc, char **argv)
 {
-  if ( argc < 2 ) {
-    fprintf(stderr, "Usage: %s elf-file (symbols)\n", argv[0]);
-    return 6;
+  if ( argc < 2 ) usage(argv[0]);
+  int opt_l = 0;
+  // process options
+  while (1)
+  {
+    auto c = getopt(argc, argv, "dl");
+     if (c == -1)
+      break;
+    if ( c == 'd' ) g_opt_d = 1;
+    else if ( c == 'l' ) opt_l = 1;
+    else usage(argv[0]);
   }
+  if (optind == argc)
+     usage(argv[0]);
   Elf64_Addr text_start = 0;
   Elf_Xword text_size = 0;
   section *text_section = NULL, *data_section = NULL, *bss_section = NULL;
   elfio reader;
-  if ( !reader.load( argv[1] ) )
+  if ( !reader.load( argv[optind] ) )
   {
-     fprintf(stderr,  "File %s is not found or it is not an ELF file\n", argv[1] );
+     fprintf(stderr,  "File %s is not found or it is not an ELF file\n", argv[optind] );
      return 1;
   }
   int has_syms = 0;
@@ -62,23 +87,23 @@ int main(int argc, char **argv)
   }
   // check that we have all sections
   if ( !text_section ) {
-    fprintf(stderr, "cannot find text section in %s\n", argv[1]);
+    fprintf(stderr, "cannot find text section in %s\n", argv[optind]);
     return 1;
   }
   if ( !data_section ) {
-    fprintf(stderr, "cannot find data section in %s\n", argv[1]);
+    fprintf(stderr, "cannot find data section in %s\n", argv[optind]);
     return 1;
   }
   if ( !has_syms )
   {
-    if ( argc < 3 ) {
-     fprintf(stderr, "symbols need for %s\n", argv[1]);
+    if ( optind + 1 == argc ) {
+     fprintf(stderr, "symbols need for %s\n", argv[optind]);
      return 2;
     }
-    int err = read_ksyms(argv[2]);
+    int err = read_ksyms(argv[optind + 1]);
     if ( err )
     {
-      fprintf(stderr, "cannot read symbols from %s, error %d\n", argv[2], err);
+      fprintf(stderr, "cannot read symbols from %s, error %d\n", argv[optind + 1], err);
       return err;
     }
   }
@@ -94,7 +119,7 @@ int main(int argc, char **argv)
     if ( addr )
       ad->add_noreturn(addr);
     else
-      fprintf(stderr, "cannot find __stack_chk_fail\n");
+      rcf("__stack_chk_fail");
     bd = ad;
   } else if ( m == EM_X86_64 )
   {
@@ -112,7 +137,6 @@ int main(int argc, char **argv)
    bd = x64;
   } else if ( m == EM_MIPS )
   {
-    g_opt_d = 1;
     mips_disasm *md = new mips_disasm(reader.get_encoding() == ELFDATA2MSB,
        reader.get_class() == ELFCLASS32 ? 32 : 64,
        text_start, text_size, text_section->get_data(),
@@ -122,7 +146,7 @@ int main(int argc, char **argv)
     if ( addr )
       md->add_noreturn(addr);
     else
-      fprintf(stderr, "cannot find __stack_chk_fail\n");
+      rcf("__stack_chk_fail");
     bd = md;
   } else {
    printf("dont know how process machine %d, endianess %d\n", m, reader.get_encoding());
@@ -133,38 +157,40 @@ int main(int argc, char **argv)
   // bpf_target
   auto entry = get_addr("bpf_iter_reg_target");
   auto mlock = get_addr("mutex_lock");
-  a64 bpf_target = 0;
   if ( !entry )
-    printf("cannot find bpf_iter_reg_target\n");
+    rcf("bpf_iter_reg_target");
   else if ( !mlock )
-    printf("cannot find mutex_lock\n");
-  else
-    bpf_target = bd->process_bpf_target(entry, mlock);
-  if ( bpf_target )
-    printf("bpf_target %p\n", bpf_target);
+    rcf("mutex_lock");
+  else {
+    auto bpf_target = bd->process_bpf_target(entry, mlock);
+    if ( bpf_target )
+     printf("bpf_target %p\n", bpf_target);
+  }
   // kfunc_set_tab offset
   auto kf = get_addr("btf_free_kfunc_set_tab");
-  int off = 0;
   if ( !kf )
-    printf("cannot find btf_free_kfunc_set_tab\n");
-  off = bd->find_kfunc_set_tab_off(kf);
-  if ( off )
-    printf("kfunc_set_tab offset %d\n", off);
+    rcf("btf_free_kfunc_set_tab");
+  else {
+   int off = bd->find_kfunc_set_tab_off(kf);
+   if ( off )
+     printf("kfunc_set_tab offset %d\n", off);
+  }
   // find trace_event_call.filter offset
-  int event_foff = 0;
   entry = get_addr("trace_remove_event_call");
   auto free_evt = get_addr("free_event_filter");
   if ( !entry )
-    printf("cannot find trace_remove_event_call\n");
+    rcf("trace_remove_event_call");
   else if ( !free_evt )
-    printf("cannot find free_event_filter\n");
-  else
-    event_foff = bd->process_trace_remove_event_call(entry, free_evt);
-  if ( event_foff )
-    printf("trace_event_call.filter offset %d\n", event_foff);
+    rcf("free_event_filter");
+  else {
+    int event_foff = bd->process_trace_remove_event_call(entry, free_evt);
+    if ( event_foff )
+      printf("trace_event_call.filter offset %d\n", event_foff);
+  }
+  // lsm
   auto list_head = get_addr("security_hook_heads");
   if ( !list_head )
-    printf("cannot find security_hook_heads\n");
+    rcf("security_hook_heads");
   else {
     bd->set_shook(list_head);
     auto test = get_addr("security_bpf_map_free");
