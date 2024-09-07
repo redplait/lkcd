297c4f1f97fb1c2fcb398929b25fecb59d83fd76
redp <redplait@gmail.com>
2024-09-07 09:39:02 +0300

    find_kmod_ex

--- test/kmods.cc
+++ test/kmods.cc
@@ -22,12 +22,14 @@ class mods_storage
   public:
     int read_mods();
     int read_from_driver(int fd);
+    int read_from_driver2(int fd);
     const char *find(unsigned long addr);
   protected:
+    void fill(unsigned long *buf);
     std::list<one_mod> m_list;
 };
 
-static mods_storage s_mod_stg;
+static mods_storage s_mod_stg, s_mod_stg_ex;
 
 const char *mods_storage::find(unsigned long addr)
 {
@@ -39,6 +41,25 @@ const char *mods_storage::find(unsigned long addr)
      return c.name.c_str();
   }
   return NULL;
+}
+
+void mods_storage::fill(unsigned long *buf)
+{
+  one_module *mod = (one_module *)(buf + 1);
+#ifdef DEBUG
+  printf("%ld modules\n", buf[0]);
+#endif
+  for ( unsigned long cnt = 0; cnt < buf[0]; cnt++, mod++ )
+  {
+    one_mod tmp;
+    tmp.name = mod->name;
+    tmp.start = (unsigned long)mod->base;
+    tmp.len = mod->size;
+#ifdef DEBUG
+    printf("%s %lX %lX\n", tmp.name.c_str(), tmp.start, tmp.len);
+#endif
+    m_list.push_back(tmp);
+  }
 }
 
 int mods_storage::read_from_driver(int fd)
@@ -67,21 +88,38 @@ int mods_storage::read_from_driver(int fd)
     free(buf);
     return errno;
   }
-  one_module *mod = (one_module *)(buf + 1);
-#ifdef DEBUG
-  printf("%ld modules\n", buf[0]);
-#endif
-  for ( unsigned long cnt = 0; cnt < buf[0]; cnt++, mod++ )
+  fill(buf);
+  free(buf);
+  return 0;
+}
+
+int mods_storage::read_from_driver2(int fd)
+{
+  unsigned long args[2] = { 0, 2 };
+  int err = ioctl(fd, IOCTL_READ_MODULES, (int *)&args);
+  if ( err )
   {
-    one_mod tmp;
-    tmp.name = mod->name;
-    tmp.start = (unsigned long)mod->base;
-    tmp.len = mod->size;
-#ifdef DEBUG
-    printf("%s %lX %lX\n", tmp.name.c_str(), tmp.start, tmp.len);
-#endif
-    m_list.push_back(tmp);
+    printf("IOCTL_READ_MODULES(2) count failed, errno %d (%s)\n", errno, strerror(errno));
+    return errno;
   }
+  if ( !args[0] ) return 0;
+  size_t size = sizeof(unsigned long) + sizeof(one_module) * args[0];
+  unsigned long *buf = (unsigned long *)malloc(size);
+  if ( !buf )
+  {
+    printf("cannot alloc %lX bytes for modules, errno %d (%s)\n", size, errno, strerror(errno));
+    return errno;
+  }
+  buf[0] = args[0];
+  buf[1] = 2;
+  err = ioctl(fd, IOCTL_READ_MODULES, (int *)buf);
+  if ( err )
+  {
+    printf("IOCTL_READ_MODULES(2) failed, errno %d (%s)\n", errno, strerror(errno));
+    free(buf);
+    return errno;
+  }
+  fill(buf);
   free(buf);
   return 0;
 }
@@ -121,9 +159,20 @@ const char *find_kmod(unsigned long addr)
   return s_mod_stg.find(addr);
 }
 
+
+const char *find_kmod_ex(unsigned long addr)
+{
+  return s_mod_stg_ex.find(addr);
+}
+
 int init_kmods(int fd)
 {
   if ( !getuid() )
     return s_mod_stg.read_mods();
   return s_mod_stg.read_from_driver(fd);
+}
+
+int init_kmods_ex(int fd)
+{
+  return s_mod_stg_ex.read_from_driver2(fd);
 }
--- test/kmods.h
+++ test/kmods.h
@@ -5,7 +5,10 @@ extern "C" {
 #endif
 
 int init_kmods(int fd);
+int init_kmod_ex(int fd);
+// return 0 if ok
 const char *find_kmod(unsigned long addr);
+const char *find_kmod_ex(unsigned long addr);
 
 #ifdef __cplusplus
 };
